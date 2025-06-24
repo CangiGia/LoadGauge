93b28954a58d5f4645dc6a8bdd3888d9ace583f7
GiacomoCangi <giacomocangi.96@gmail.com>
2025-06-24 11:26:30 +0200

    StrainGauge and Murammim class initialized

---
+++ src/temporary.py
@@ -0,0 +1,25 @@
+"""
+Temporary file to test the developed code.
+This file is not intended for production use and should be deleted after testing.
+It is used to verify the functionality of the code snippets and configurations.
+
+Author: Giacomo Cangi
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("\n\t... all packages correctly imported ... \n")
+
+class StrainGauge:
+    def __init__(self):
+        print("\t... StrainGauge class correctly initialized ... \n")
+
+class Murammim:
+    def __init__(self):
+        print("\t... Constructor class correctly initialized ...\n")
+
+monoaxial_strain_gauge = StrainGauge()
+reconstructor = Murammim()
+
+print("\t--> works until here <-- \n")
